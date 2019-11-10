a7ee368e6a47026cc95157bed6fbff8f70b1ecd8
Bruno Parolini <brunoparolini@gmail.com>
2019-11-09 22:01:23 -0300

    First version. Very simple tests.

---
+++ mp3_image_extract.py
@@ -0,0 +1,37 @@
+import os
+from mutagen.id3 import ID3
+
+
+def save_folder_jpg(mp3_file):
+    file = open(mp3_file, 'rb')
+    mp3 = ID3(file)
+    apic_data = mp3.getall("APIC")
+    if len(apic_data) >= 1:
+        image_file = open(os.path.join(os.path.dirname(mp3_file), "folder.jpg"), "w+b")
+        image_file.write(apic_data[0].data)
+        image_file.close()
+
+
+def navigate_directory(directory):
+    sub_directory = ""
+    for directory_name in os.listdir(directory):
+        sub_directory = os.path.join(directory, directory_name)
+        if os.path.isdir(sub_directory):
+            navigate_directory(sub_directory)
+        else:
+            if find_folder_jpg(directory):
+                return
+
+    if sub_directory.endswith(".mp3"):
+        save_folder_jpg(sub_directory)
+
+
+def find_folder_jpg(directory):
+    for file in os.listdir(directory):
+        if file == "folder.jpg":
+            return True
+
+    return False
+
+
+navigate_directory("/home/bparolini/Downloads/2018 - Xenoblade Chronicles 2/")
